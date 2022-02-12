dae640e0bfcdae117d0d536357eedb86cc79c3b9
Carlos Hernandez Topete <72361463+toshiroto@users.noreply.github.com>
2022-02-12 18:06:44 +0900

    JSK Practice Practice

--- src/index.js
+++ src/index.js
@@ -6,3 +6,16 @@
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Carlos";
+var currentYear = new Date().getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>created by {name}</p>
+    <p>Copyright {currentYear}</p>
+  </div>,
+  document.getElementById("root")
+);
